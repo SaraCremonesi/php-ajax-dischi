32b3bf2eb83235f986246c879a8d199a239b1c8b
SaraCremonesi <63293011+SaraCremonesi@users.noreply.github.com>
2020-11-01 21:42:17 +0100

    aggiornamento

--- server.php
+++ server.php
@@ -10,25 +10,3 @@
 
   // Stampo a schermo l'array di oggetti/JSON
   echo $databaseJson;
-
-  //PER FILTRI
-  // Recupero il valore passato in GET con la chaimata AJAX nella funzione filteredCds() nel file JavasScript;
-  $author = $_GET["author"];
-
-  // Creo l'array vuoto
-  $filtred_database = [];
-
-  // Ciclo sul database e se trovo corrispondenze con il valore passato in $_GET lo metto nell'array
-  foreach ($database as $singoloCd) {
-    if ($author == $singoloCd['author']) {
-      $filtred_database[] = $singoloCd;
-    }
-  }
-
-  // Converto il nuovo array in JSON e lo metto nella variabile
-  $filtred_cds = json_encode($filtred_database);
-
-  header('Content-Type: application/json');
-
-  echo $filtred_cds;
- ?>
