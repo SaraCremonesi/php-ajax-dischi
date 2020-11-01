ed6c871460492bf65eab0b26e1a16e401a361430
SaraCremonesi <63293011+SaraCremonesi@users.noreply.github.com>
2020-11-01 18:48:32 +0100

    commenti aggiunti

--- server.php
+++ server.php
@@ -1,6 +1,13 @@
 <?php
+  // includo il mio database
   include __DIR__ . '/database.php';
+
+  // converto l'array database in JSON
   $databaseJson = json_encode($database);
+
+  // communico al server che il tipo di contenuto è json
   header('Content-Type: application/json');
+
+  // satmpo a schermo l'array di oggetti/JSON
   echo $databaseJson;
  ?>
